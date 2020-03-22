77b40838b055154416bf86e2d9195fb6e5a6cdd3
mes.inc <kemboivictor@outlook.com>
2020-03-22 23:44:55 +0300

    Review form with validation.

--- main.js
+++ main.js
@@ -1,3 +1,89 @@
+Vue.component(
+    'product-review',
+    {
+        template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+            <p v-if="errors.length">
+                <b> Please corrrect the following error(s):</b>
+                <ul>
+                    <li v-for ="error in errors">
+                        {{error}} </li>
+                        </ul>
+                        </p>
+
+        <p>
+          <label for="name">Name:</label>
+          <input id="name" v-model="name" placeholder="name">
+        </p>
+        
+        <p>
+          <label for="review">Review:</label>      
+          <textarea id="review" v-model="review" ></textarea>
+        </p>
+        
+        <p>
+          <label for="rating">Rating:</label>
+          <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+          </select>
+        </p>
+
+        <p>
+        Would you recommend this product to someone else? </p>
+
+        <input type="radio" id="yes" name="recommend"
+         value="yes" v-model = "recommendation">
+        <label for="yes">Yes</label><br>
+        <input type="radio" id="no" name="recommend"
+         value="no" v-model = "recommendation">
+        <label for="no">No</label><br>
+            
+        <p>
+          <input type="submit" value="Submit">  
+        </p>    
+      
+      </form>
+        `,
+        data(){
+            return {
+                name: null,
+                review: null,
+                rating: null,
+                errors:[],
+                recommendation: null
+            }
+        },
+        methods:{
+            onSubmit: function(){
+                this.errors = []
+                if(this.name && this.review && this.rating && this.recommendation){
+                    let productReview = {
+                        name : this.name,
+                        review: this.review,
+                        rating: this.rating
+                    }
+    
+    
+                    this.$emit('review-submitted', productReview)
+                    this.name = null
+                    this.review = null
+                    this.rating = null
+                    this.recommendation = null
+                }else{
+                    if(!this.name) this.errors.push("Name required!")
+                    if(!this.review) this.errors.push("Review required!")
+                    if(!this.rating) this.errors.push("Rating required!")
+                    if(!this.recommendation) this.errors.push("Recommendation required!")
+                }
+                
+            }
+        }
+    }
+)
 
 Vue.component(
     'product-details',
@@ -78,11 +164,25 @@ Vue.component('product',{
                      :disabled ="!inStock" :class="{disabledButton:!inStock}">
                      Add to Cart</button>
 
-                    <div class="cart">
-                        <p>Cart({{cartNo}})</p>
-                    </div>
+                   
 
                     <button v-on:click="removeFromCart">Remove from Cart</button>
+
+                    <div>
+
+                        <h2>Reviews</h2>
+                        <p v-if="!reviews.length">There are no reviews yet!</p>
+                        <ul>
+                                <li v-for="review in reviews">
+                                   <p>Name:  {{review.name}}</p>
+                                   <p>Rating:  {{review.rating}}</p>
+                                   <p>Review:  {{review.review}}</p>
+                                </li>
+                                </ul>
+
+                    </div>
+
+                    <product-review @review-submitted = "addReview"></product-review>
 
                     
                    
@@ -168,11 +268,9 @@ Vue.component('product',{
                     size: "x-large"
                 },
             ],
-            cart: {
-               items:[]
-            },
-            cartNo:0,
-            activeVariant:null
+           
+            activeVariant:null,
+            reviews: []
         }
 
     } ,
@@ -181,35 +279,7 @@ Vue.component('product',{
         addToCart: function(){
 
             //check if variant in cart and cart items is not zero
-
-            let variant = this.activeVariant
-
-            if(this.cart.items.length>0 && this.cart.items.includes(this.activeVariant)){
-                variant.cart += 1;
-                variant.amount -= 1;
-
-               this.cart.items.find(item=>{
-                   if(item.variantId == variant.variantId){
-                    item = variant
-                   }
-               })
-
-            }else{
-                variant.amount -= 1;
-                variant.cart += 1;
-                this.cart.items.push(
-                    variant
-                )
-            }
-
-            
-
-        
-            if(variant.amount == 0){
-                this.inStock = false;
-            }
-
-            this.getCartNo()
+        this.$emit('add-to-cart', this.activeVariant)
           
 
 
@@ -225,64 +295,16 @@ Vue.component('product',{
             }
         },
         removeFromCart: function(){
-
-            let variant = this.activeVariant
-            //check if items in cart
-            if(this.cart.items.length >0){
-               //find active variant
-                this.cart.items.find(
-                    item =>{
-                        
-                        if(variant.variantId == item.variantId){
-
-                            if(variant.cart>0){
-                                variant.cart -= 1;
-                                variant.amount += 1;
-
-                                item = variant
-
-                                this.variants.find(item=>{
-                                    if(item.variantId == variant.variantId){
-                                       item = variant
-                                    }
-                                })
-                            }
-                            
-                    
-                        }
-                    }
-                )
-
-               
-
-              
-                if(this.activeVariant.amount >0){
-                    this.inStock = true
-                }else{
-                    this.inStock = false
-                }
-                
-            }
-            
-
-            this.getCartNo()
+                this.$emit("remove-from-cart", this.activeVariant)
            
         },
         initActiveVariant: function(){
             this.activeVariant = this.variants[0]
         },
-        getCartNo: function(){
-            let cartNoo = 0  
-            this.cart.items.map(
-                
-                item =>{
-                    cartNoo += item.cart
-
-                }
-            )
-
-           this.cartNo = cartNoo
+        addReview(productReview){
+            this.reviews.push(productReview)
         }
+      
     },
     beforeMount(){
         this.initActiveVariant()
@@ -304,7 +326,102 @@ Vue.component('product',{
 var app =  new Vue({
     el: '#app',
     data:{
-        premium: true
+        premium: true,
+        cart: {
+            items:[]
+         },
+         cartNo:0,
+    },
+    methods:{
+        updateCart: function(variant){
+            
+           if(variant.amount>0){
+
+          
+
+            if(this.cart.items.length>0 && this.cart.items.includes(variant)){
+
+                
+                variant.cart += 1;
+                variant.amount -= 1;
+
+               this.cart.items.find(item=>{
+                   if(item.variantId == variant.variantId){
+                    item = variant
+                   }
+               })
+
+            }else{
+                variant.amount -= 1;
+                variant.cart += 1;
+                this.cart.items.push(
+                    variant
+                )
+            }
+
+        }else{
+            this.inStock = false;
+        }
+
+        
+    
+
+            this.getCartNo()
+        },
+        getCartNo: function(){
+            let cartNoo = 0  
+            this.cart.items.map(
+                
+                item =>{
+                    cartNoo += item.cart
+
+                }
+            )
+
+           this.cartNo = cartNoo
+        },
+        removeInCart: function(variant){
+           
+            //check if items in cart
+            if(this.cart.items.length >0){
+               //find active variant
+                this.cart.items.find(
+                    item =>{
+                        
+                        if(variant.variantId == item.variantId){
+
+                            if(variant.cart>0){
+                                variant.cart -= 1;
+                                variant.amount += 1;
+
+                                item = variant
+
+                            /*   this.variants.find(item=>{
+                                    if(item.variantId == variant.variantId){
+                                       item = variant
+                                    }
+                                }) */
+                            }
+                            
+                    
+                        }
+                    }
+                )
+
+               
+
+              
+                if(variant.amount >0){
+                    this.inStock = true
+                }else{
+                    this.inStock = false
+                }
+                
+            }
+            
+
+            this.getCartNo()
+        }
     }
     
 })
