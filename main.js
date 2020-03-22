42eb9448e917fc9693adb8dc0fbbbac3e8a46e30
mes.inc <kemboivictor@outlook.com>
2020-03-22 20:23:28 +0300

    added product components

--- main.js
+++ main.js
@@ -1,84 +1,145 @@
-var app =  new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        description: 'Off-white funky socks.',
-        image:"./assets/img/green_sock.png",
-        link: "https://www.vuejs.org",
-        inStock: true,
-        details:["80% cotton","20% Polyester", "Gender-neutral"],
-        variants:[
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: "./assets/img/green_sock.png",
-                cart:0,
-                amount: 8,
-                size: "small"
+
+
+
+Vue.component('product',{
+    template: `
+    <div class="product">
+                <div class="product-image">
+                    <img v-bind:src="image"/>
+                </div>
+
+                <div class="product-info">
+                
+
+                    <h1>{{product}}</h1>
+                   
+                
+                    <p v-if="inStock">In Stock.</p>
+                    <p  v-else :class="{outOfStock : !inStock}"
+                     >
+                     
+                            Out of Stock! </p>
+
+                    <ul>
+                        <li v-for="detail in details">{{detail}}</li>
+                        <li>Size: {{
+                            activeVariant.size
+                        }}</li>
+                    </ul>
+                    <div v-for="variant in variants" 
+                    :key="variant.variantId"
+                    class="color-box"
+                    :style = "{backgroundColor: variant.variantColor}"
+                    @mouseOver="updateProduct(variant)"
+                    >
+                    
+                    <p style="color: white;">{{variant.amount}}</p>
+                    </div>
+                    
+
+                    
+
+                    <button v-on:click="addToCart()"
+                     :disabled ="!inStock" :class="{disabledButton:!inStock}">
+                     Add to Cart</button>
+
+                    <div class="cart">
+                        <p>Cart({{cartNo}})</p>
+                    </div>
+
+                    <button v-on:click="removeFromCart">Remove from Cart</button>
+
+                    
+                   
+                </div>
+
+               
+
+        </div>
+    `,data()
+    {
+        return {
+            product: 'Socks',
+            description: 'Off-white funky socks.',
+            image:"./assets/img/green_sock.png",
+            link: "https://www.vuejs.org",
+            inStock: true,
+            details:["80% cotton","20% Polyester", "Gender-neutral"],
+            variants:[
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: "./assets/img/green_sock.png",
+                    cart:0,
+                    amount: 8,
+                    size: "small"
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "green",
+                    variantImage: "./assets/img/green_sock.png",
+                    cart:0,
+                    amount: 12,
+                    size: "medium"
+                },
+                {
+                    variantId: 2236,
+                    variantColor: "green",
+                    variantImage: "./assets/img/green_sock.png",
+                    amount: 14,
+                    cart:0,
+                    size: "large"
+                },
+                {
+                    variantId: 2237,
+                    variantColor: "green",
+                    variantImage: "./assets/img/green_sock.png",
+                    amount: 15,
+                    cart:0,
+                    size: "x-large"
+                },
+                {
+                    variantId: 2238,
+                    variantColor: "blue",
+                    variantImage: "./assets/img/blue_sock.png",
+                    amount: 8,
+                    cart:0,
+                    size: "small"
+                },
+                {
+                    variantId: 2239,
+                    variantColor: "blue",
+                    variantImage: "./assets/img/blue_sock.png",
+                    amount: 12,
+                    cart:0,
+                    size: "medium"
+                },
+                {
+                    variantId: 2240,
+                    variantColor: "blue",
+                    variantImage: "./assets/img/blue_sock.png",
+                    amount: 14,
+                    cart:0,
+                    size: "large"
+                },
+                {
+                    variantId: 2241,
+                    variantColor: "blue",
+                    variantImage: "./assets/img/blue_sock.png",
+                    amount: 15,
+                    cart:0,
+                    size: "x-large"
+                },
+            ],
+            cart: {
+               items:[]
             },
-            {
-                variantId: 2235,
-                variantColor: "green",
-                variantImage: "./assets/img/green_sock.png",
-                cart:0,
-                amount: 12,
-                size: "medium"
-            },
-            {
-                variantId: 2236,
-                variantColor: "green",
-                variantImage: "./assets/img/green_sock.png",
-                amount: 14,
-                cart:0,
-                size: "large"
-            },
-            {
-                variantId: 2237,
-                variantColor: "green",
-                variantImage: "./assets/img/green_sock.png",
-                amount: 15,
-                cart:0,
-                size: "x-large"
-            },
-            {
-                variantId: 2238,
-                variantColor: "blue",
-                variantImage: "./assets/img/blue_sock.png",
-                amount: 8,
-                cart:0,
-                size: "small"
-            },
-            {
-                variantId: 2239,
-                variantColor: "blue",
-                variantImage: "./assets/img/blue_sock.png",
-                amount: 12,
-                cart:0,
-                size: "medium"
-            },
-            {
-                variantId: 2240,
-                variantColor: "blue",
-                variantImage: "./assets/img/blue_sock.png",
-                amount: 14,
-                cart:0,
-                size: "large"
-            },
-            {
-                variantId: 2241,
-                variantColor: "blue",
-                variantImage: "./assets/img/blue_sock.png",
-                amount: 15,
-                cart:0,
-                size: "x-large"
-            },
-        ],
-        cart: {
-           items:[]
-        },
-        cartNo:0,
-        activeVariant:null
-    },
+            cartNo:0,
+            activeVariant:null
+        }
+
+    } ,
+
     methods:{
         addToCart: function(){
 
@@ -189,4 +250,12 @@ var app =  new Vue({
     beforeMount(){
         this.initActiveVariant()
      }
+
+}
+)
+
+
+var app =  new Vue({
+    el: '#app'
+    
 })
